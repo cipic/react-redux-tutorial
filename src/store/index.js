b94fb844e3ae01c74ec89015cff511267b60b5bf
cipic <andrei.ciprian1@gmail.com>
2025-10-03 00:38:44 +0300

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const songSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songSlice.reducer,
+  },
+});
+
+export { store };
+export const { addSong, removeSong } = songSlice.actions;
